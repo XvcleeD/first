6f55e347f4afc22e3f4288835f2cf41f0a898dae
XvcleeD <Xvclee255@gmail.com>
2022-11-29 17:51:41 +0800

    js day 2

---
+++ main/js/cond.js
@@ -0,0 +1,77 @@
+// let age = prompt('tanii nas hed be?')
+// if (age >= 18){
+//     alert('Nasand hursen')
+// }
+// else{
+//     alert('Nasand huregui')
+// }
+// const date = new Date()
+// const time = date.getHours()
+
+// console.log(time)
+// if ( 1 > 1){
+//     console.log('first if')
+// } else if (2 > 1){
+//     console.log('second if')
+// } else{
+//     console.log('last if')
+// }
+// let user = prompt( 'tanii ner')
+// // const date = new Date()
+// // const time = date.getHours()
+// let time = prompt ( 'tsagaa oruuln uu' )
+// if ( 6 >= time ){ 
+//     alert(' uuriin mend ' + user )
+// } else if ( 10 >= time  ){
+//     alert('ogloonii mend ' + user )
+// } else if ( 18 >= time ){
+//     alert ('odriin mend ' + user )}
+// else {
+//     alert('oroin mend ' + user )
+// }
+// const date = new Date
+// const time = date.getHours()
+
+// let username = prompt('What is your name?')
+// let greeting 
+
+// if ( time < 12 )
+// {
+//     greeting = 'Goood moring'
+// }
+// else if ( time > 12 && time < 18 )
+// {
+//     greeting = 'Good afternoon'
+// }
+// else 
+// {
+//     greeting = 'Good evening'
+// }
+
+// console.log(greeting, username)
+
+// let age = 21
+// let message = age >= 18 ? 'Ta nasand hursn' : ' ta huuhdeeree bainaa' 
+// console.log (message)
+// message = age >= 18 ? true ; false
+
+// let too = prompt('toogoo oruuln uu?')
+// let chanar = (too % 2)==0 ? 'tegsh too' : ' sondgoi too' 
+// alert (chanar)
+
+
+let year = prompt('on oo uruuln uu ? ')
+let message = ( year % 4 || (year % 400) == 0 && (year % 100) == 0) == 0 ? 'ondor jil' : 'ondor jil bish'
+alert(message)
+console.log(message)
+
+// let year = prompt('on oo uruuln uu ? ')
+
+// if ( (year % 4) == 0 || (year % 400) == 0 && (year % 100) == 0){
+//     alert('onder jil')
+//     console.log('odor jil')
+// }
+// else{
+//     alert('ondor jil bish')
+//     console('ondor jil bish')
+// }
--- main/js/main.js
+++ main/js/main.js
@@ -6,3 +6,5 @@ let y = 2
 let x = 2
 m = (y * 2 - y * 1) /(x * 2 - x * 1)
 console.log(m)
+
+if( age = b);
