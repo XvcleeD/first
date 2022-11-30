bec45ee06a05e803bdd151d83d795a9ef00b4872
XvcleeD <Xvclee255@gmail.com>
2022-11-30 18:34:06 +0800

    js 3day

---
+++ main/js/answer.js
@@ -0,0 +1,36 @@
+// question
+// const answer1 = 'Ulaanbatar'
+// const answer2 = 'Hobsgol' 
+
+// const question1 = prompt('Mongol ulsiin niislel ?')
+// const question2 = prompt('Mongoliin hamgiin tom tsengeg nuur ?')
+
+
+
+// alert(message2)
+
+// const max = prompt('please, pvovide a max number')
+// const min = prompt('please, pvovide a min number')
+
+// const too =Math.floor(Math.random() * (Number(max) - Number(min) + 1) + Number(min)  ) 
+
+// console.log(too)
+
+
+const register = prompt('регистрийн дугаар')
+
+str = register.length
+let too = register.substring(2, 10)
+let useg = register.substring(0, 2)
+
+if ( 10 == str) {
+    if (  )
+}
+else if {
+
+}
+else{
+    alert('register dutuu baina')
+}
+
+
