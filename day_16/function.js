0fe9157397d675d92e1c56ad8e2478836171008b
XvcleeD <Xvclee255@gmail.com>
2022-12-06 20:47:37 +0800

    js_day_16

---
+++ day_16/function.js
@@ -0,0 +1,226 @@
+// console.log('function')
+// // 1. f(x , y) = x'2 + y'2
+// function f1(x ,y) {
+//     console.log(Math.pow(x, 2) + Math.pow(y, 2));
+    
+// }
+// f1(3, 4)
+// function f2(x) {
+//     console.log(Math.pow(x, Math.E))
+    
+// }
+// f2(2)
+// function f3(a, b) {
+//     console.log((Math.pow(a ,2) - Math.pow(b, 2) ))
+    
+// }
+// f3(3, 2)
+// function f50(a, b) {
+//     console.log((a - b) * (a + b))
+    
+// }
+// f50(3, 2)
+
+// function f4(a, b) {
+//     console.log(Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)))
+    
+// }
+// f4(3, 4)
+
+// function f51(y) {
+//     console.log(Math.pow(y, 2) - 2*y + 4)
+    
+// }
+// f51(4, 3)
+
+// // let a = [3]
+// // let b = [4]
+// // console.log(a - b * (Math.pow(a, 2) + Math.pow(b, 2) - a))
+
+
+// let  f5 = (a, b) =>{
+//     console.log(a - b * (Math.pow(a, 2) + Math.pow(b, 2) - a))
+
+// }
+// f5(3, 4)
+
+// // F(a,b ) = 2 * (a + b)
+// function f6(a, b) {
+//     console.log(2 * (a + b))
+    
+// }
+// f6( 4, 5)
+
+// function f7(a, b) {
+//     console.log( Math.pow((a - b), 2))    
+// }
+// f7(4, 5)
+
+// function f8(a, b ) {
+//     console.log(a * b)
+    
+// }
+// f8(8, 5)
+
+// function f9(a, b){
+//     console.log(3 * Math.pow((a * b), 2))
+// }
+// f9( 1, 2)
+
+// let f10 = (x, y) => {
+//     console.log(3 * Math.pow(x, 3) - 2 * Math.pow(y, 3))
+// }
+// f10( 4, 5)
+
+
+
+// function factorial(x) {
+//     let multiplicapion = 1;
+//     for ( let i = 1; i <= x; i++ ){
+//         multiplicapion = multiplicapion * i
+//     }
+//     console.log(multiplicapion)
+// }
+// factorial(5);
+
+// function myFunction(p1, p2) {
+//     const sum = p1 + p1 ;
+//     return sum;  
+    
+// }
+// console.log(myFunction(2, 3));
+
+
+//  find out the even number 
+
+// function  isEven(number) {
+//     let result = '';
+//     if(number % 2 == 0 ){
+//         result = 'its even number'
+//         // console.log('its even number')
+//     } else { 
+//         result = 'its odd number'
+//         // console.log('its odd number')
+//     }
+//     return result
+// } 
+// console.log(isEven(5));
+// isEven(4);
+// isEven(18);
+
+
+
+// 
+
+
+// function findMax(a, b) {
+//     if(a > b){
+//         return a; 
+//     }
+//     else { 
+//         return b;
+//     }
+// }
+
+// console.log(findMax(1, 3));
+// console.log(findMax(100, 3));
+// console.log(findMax(100, 100));
+
+// 
+
+
+
+// function expression
+
+
+// let findMax = function(a, b) {
+//     if(a > b){
+//         return a; 
+//     }
+//     else { 
+//         return b;
+//     }
+// }
+// console.log(findMax(1, 3));
+// console.log(findMax(100, 3));
+// console.log(findMax(150, 50));
+
+// function expression => arrow function
+
+
+
+// let findMax = (a, b) => {
+//     if (a > b){
+//         return a;
+//     }
+//     else {
+//         return b;
+//     }
+// }
+// console.log(findMax(1, 3));
+// console.log(findMax(100, 3));
+// console.log(findMax(110, 100));
+
+
+// Array sorter function 
+
+// let max = -1000;
+// const arr = [4, 32, 2, 5, 8, -10, 20, 1, 0, -20]
+
+// arr.sort(function(a, b){
+//     return a - b 
+// })
+// let arrayMax = (array) => {
+//     for ( let i  = 0; i < array.length; i++){
+//         if (max <= array[i]){
+//             max = array[i];
+//         }
+//     }
+// }
+// arrayMax(arr)
+// console.log(arr)
+// console.log(max);
+
+
+// simple array-1 
+
+
+let itCompanies =['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon' ]
+
+// 1.
+console.log(itCompanies)
+console.log('\n')
+// 2.
+console.log(itCompanies.length)
+console.log('\n')
+// .3`
+let urt = itCompanies.length
+let i
+i = Math.floor(itCompanies.length / 2)
+if (itCompanies.length % 2 != 0){
+    
+    console.log(itCompanies[0], itCompanies[i], itCompanies[itCompanies.length -1])
+    
+}
+else {
+    
+    console.log(itCompanies[0], itCompanies[i-1], itCompanies[i], itCompanies[itCompanies.length -1])
+}
+
+console.log('\n')
+// 4.
+
+for(let i = 0; i < itCompanies.length; i++){
+    console.log(itCompanies[i])
+
+}
+console.log('\n')
+// 5.
+for(let i = 0; i < itCompanies.length; i++){
+    console.log(itCompanies[i].toUpperCase())
+
+}
+
+
+
+
---
+++ day_16/main.js
@@ -0,0 +1,37 @@
+console.log('date and funtion')
+
+let now = new Date();
+console.log( now )
+
+// let startPCTime = new Date(0);
+// consule.log( startPCTime );
+
+let Dec31_1969 = new Date(-24 * 3600 * 1000);
+console.log( Dec31_1969); 
+
+let date = new Date("2017-01-26")
+console.log(date);
+
+
+
+
+
+
+
+
+
+
+// assignment ongortal hededen odor tas min baigaa bee
+
+
+let tuesday = new Date();
+console.log(tuesday)
+
+let lastDayOf2022 = new Date('2022-12-31')
+console.log(lastDayOf2022)
+
+console.log(tuesday.getDate())
+console.log(lastDayOf2022.getDate())
+
+console.log(Math.abs(lastDayOf2022.getDate() - tuesday.getDate() ))
+console.log (Math.abs(lastDayOf2022.getHours() - tuesday.getHours()))
