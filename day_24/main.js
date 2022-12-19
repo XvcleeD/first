781cc7570471047cd4782ef044fbe22a296db8a7
XvcleeD <Xvclee255@gmail.com>
2022-12-19 16:58:49 +0800

    day_

---
+++ day_24/main.js
@@ -0,0 +1,22 @@
+let min = 00
+let sec = 00 
+let diol = 00 
+
+let timeDiol =document.getElementById("doil")
+let timeSec = document.getElementById("sec")
+let timeMin = document.getElementById("min")
+let starButton = document.getElementById('start')
+    
+function startTime(){
+    diol++
+    if(diol > 9){
+        timeDiol.innerHTML = "0" + diol
+    }
+    else{
+        timeDiol.innerHTML = diol
+    }
+
+
+
+}
+
