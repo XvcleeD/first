edd62bcbbe5aba1b1cda4a2745b5518183cd4483
huslee <Xvclee2551@gmail.com>
2022-12-26 19:39:52 +0800

    day_29..

--- template/main.js
+++ template/main.js
@@ -1,7 +1,37 @@
-
+let cardEl = document.getElementsByClassName('card-columns')
 
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then((res) => res.json())
-.then((data) => 
-    let divEl = document.createElement('div')
+.then((data) => {
+
+fetch('https://jsonplaceholder.typicode.com/photos')
+.then((reso) => reso.json())
+.then((datas) => {
+    console.log(datas[0].thumbnailUrl)
+    cardEl[0].innerHTML = `
+    <div class="card">
+            <div class="row">
+                <div class="col-md-5 wrapthumbnail">
+                    <a href="post.html">
+                        <div class="thumbnail" style="background-image: url('${datas[0].thumbnailUrl}');">
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-7">
+                    <div class="card-block">
+                        <h2 class="card-title"><a href="post.html"></a></h2>
+                        <h4 class="card-text"></h4>
+                        <div class="metafooter">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+    
+})
+
+
+}
+    
 )
