ef3a01a166046b180553bc5d0a36277b2ba3f5fd
huslee <Xvclee2551@gmail.com>
2022-12-27 14:17:44 +0800

    home

--- template/main.js
+++ template/main.js
@@ -1,4 +1,5 @@
-let cardEl = document.getElementsByClassName('card-columns')
+const cardEl = document.getElementsByClassName('card-columns')
+
 
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then((res) => res.json())
@@ -7,31 +8,29 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 fetch('https://jsonplaceholder.typicode.com/photos')
 .then((reso) => reso.json())
 .then((datas) => {
-    console.log(datas[0].thumbnailUrl)
-    cardEl[0].innerHTML = `
-    <div class="card">
-            <div class="row">
-                <div class="col-md-5 wrapthumbnail">
-                    <a href="post.html">
-                        <div class="thumbnail" style="background-image: url('${datas[0].thumbnailUrl}');">
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-7">
-                    <div class="card-block">
-                        <h2 class="card-title"><a href="post.html"></a></h2>
-                        <h4 class="card-text"></h4>
-                        <div class="metafooter">
+    let n = 5
+    for (let index = 0; index < n; index++) {
+        const divEl = document.createElement('div')
+        divEl.innerHTML = `
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-5 wrapthumbnail">
+                        <a href="post.html">
+                            <div class="thumbnail" style="background-image: url('${datas[index].thumbnailUrl}');">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="card-block">
+                            <h2 class="card-title"><a href="post.html">${data[index].title}</a></h2>
+                            <h4 class="card-text">${data[index].body}</h4>
+                            <div class="metafooter">
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    `
-    
+            </div>`
+        cardEl.append(divEl)
+        }
+    })
 })
-
-
-}
-    
-)
